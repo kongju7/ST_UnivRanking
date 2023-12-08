82388778ac6d3269245bd51995cb5e166b2ea26a
Ju <kongju7@korea.ac.kr>
2023-12-08 17:32:47 +0900

    ST for UnivRanking: init

---
+++ Home.py
@@ -0,0 +1,21 @@
+import streamlit as st 
+
+st.set_page_config(
+    page_title = "QS2024",
+)
+
+
+st.markdown(
+    """
+
+# QS 세계대학평가 2024년 시각화
+
+고려대학교 디지털정보처에 제공합니다.
+
+- [ ] [World](/World)   
+- [ ] [Asia](/Asia)   
+- [ ] [Korea](/Korea)   
+- [ ] [KU](/KU)     
+    """
+)
+
